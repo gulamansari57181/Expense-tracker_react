c405aa9732c2bfa2808b9a0882807bdbd6d000cb
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2023-01-14 12:28:19 +0530

    Created first custom component- ExpenseItems.js

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,18 @@
+// Importing ExpensItem component
+import ExpenseItems from "./components/ExpenseItems";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <h1>Ab aayega na maza bidu !!!</h1>
+      <p>Ye bhi visible hai !!</p>
+      <ExpenseItems></ExpenseItems>
+      <ExpenseItems></ExpenseItems>
+      <ExpenseItems></ExpenseItems>
+      <ExpenseItems></ExpenseItems>
+      <ExpenseItems></ExpenseItems>
+      <ExpenseItems></ExpenseItems>
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+
+// Creating a expense item components
+function ExpenseItems(){
+
+    return(
+        <div>
+           <h1>Expense Item</h1> 
+        
+        </div>
+    )
+}
+// Exporting by default metjod so that it can be used in another components by importing
+
+export default ExpenseItems;
