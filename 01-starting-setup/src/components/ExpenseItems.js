821ab69b50b6ef49b921a2b8cd80fc0892638d7b
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2023-01-14 18:06:26 +0530

    Implemented props to pass data to ExpenseUtems component

--- 01-starting-setup/src/components/ExpenseItems.js
+++ 01-starting-setup/src/components/ExpenseItems.js
@@ -1,13 +1,22 @@
+import "./ExpenseItems.css"
+
 
 // Creating a expense item components
-function ExpenseItems(){
+function ExpenseItems(props) {
+    // To create date object
+// const expenseDate = new Date("2023-01-14");
+// const expenseTitle="Car Insurance"
+// const expenseAmount="$250.60"
 
-    return(
-        <div>
-           <h1>Expense Item</h1> 
-        
-        </div>
-    )
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 // Exporting by default metjod so that it can be used in another components by importing
 
